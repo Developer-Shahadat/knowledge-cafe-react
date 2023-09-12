99ecead5368ad991f3e0a8aab2edb817dd616351
Developer-Shahadat <shahadatbci95@gmail.com>
2023-09-12 20:32:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Blogs from "./components/Blog/Blogs"
+import Bookmarks from "./components/Bookmarks/Bookmarks"
+import Header from "./components/Header/Header"
+
+function App() {
+
+
+  return (
+    <>
+     
+      <Header></Header>
+      <div className="md:flex container mx-auto">
+      <Blogs></Blogs>
+      <Bookmarks></Bookmarks>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,11 @@
+
+
+const Bookmarks = () => {
+    return (
+        <div className="md:w-1/3">
+            <h4 className="text-4xl">BookMarks : B</h4>
+        </div>
+    );
+};
+
+export default Bookmarks;
